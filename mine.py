02b8db853ba630913666f1bfa14ad819a036d133
Felipe Acosta <felipeacosta121204@gmail.com>
2024-04-15 20:45:54 -0400

    agrego el archivo mine

---
+++ mine.py
@@ -0,0 +1,80 @@
+import os
+
+
+
+os.system("cls")
+
+
+#ciclo repeticion for -> para
+
+# for x in range(3) :
+
+#  print(f"se portaton mal el sabado {x}")
+
+
+
+  
+
+# contadornota = 0 
+
+# try:
+
+#  cantidad = int(input("ingrese cantidad de notas a promediar\n"))
+
+#  for x in range(cantidad):
+
+#    nota = float(input("ingrese nota {x+1} \n"))
+
+    
+
+#    contadornota = contadornota + nota
+
+#  promedio = round(contadornota/cantidad)  #round(cantidadnotas/cantidad)
+
+#  promedioredondeado = round(promedio,1)
+
+#  print(f"el resultado de las {cantidad} notas es: {promedioredondeado}") 
+
+# except :
+
+#  print("tipo de dato no compatible")
+
+
+
+
+
+#while -> mientras
+
+# bandera = True
+
+
+
+# while bandera :
+
+#  numero = int(input("ingrese un numero"))
+
+#  if numero%2==0 :
+
+#    print("puede elegir otro numero!")
+
+#  else:
+
+#    print("el numero es impar, se acabo el ciclo")
+
+#    bandera = False
+
+    
+
+# print("muchas gracias por ocupar esta gran aplicacion")
+
+
+
+
+
+a = 10
+
+while a >= 10:
+
+  a = int(input("ingrese numero"))
+
+print("baicito")
